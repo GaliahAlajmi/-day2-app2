fe18a419389f086098a57b8941c31a4f35b0e1ec
GaliahAlajmi <gahlah2005@gmail.com>
2022-07-30 13:44:17 +0300

    Initial Commit

---
+++ day2 app3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day2 app3
+//
+//  Created by GALIAH ALAJMI on 01/01/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day2 app3/day2_app3App.swift
@@ -0,0 +1,17 @@
+//
+//  day2_app3App.swift
+//  day2 app3
+//
+//  Created by GALIAH ALAJMI on 01/01/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct day2_app3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
